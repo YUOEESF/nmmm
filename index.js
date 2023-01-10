1de45c8dc680c222121fd528b448ad9671c7908f
YUOEESF <121518927+YUOEESF@users.noreply.github.com>
2023-01-10 12:17:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function qsma(num1,num2){
+    return num1/num2;
+}
+
+function tarh(num1,num2){
+    return num1-num2;
+}
+
+function darb(num1,num2){
+    return num1*num2;
+}
+
+function sum(num1,num2){
+    return num1+num2;
+}
+
+
+
+function calculater (num1,num2,loay){
+    return loay (num1,num2);
+}
+console.log(tarh(6,4));
+console.log(calculater(20,6,sum))
